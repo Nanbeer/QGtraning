da1956bf30280a0b2817c425153bd773921515b9
Nanbeer <161316818+Nanbeer@users.noreply.github.com>
2024-03-23 15:40:42 +0800

    1

---
+++ 大组/week2/栈(calculator)/代码和头文件/head/FileHead.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+struct Node {//链表
+	char data;
+	Node* next;
+	Node(char c) : data(c), next(nullptr) {}
+};
+
+class Stack {//栈
+private:
+	Node* top;
+
+public:
+	Stack() : top(nullptr) {}
+	~Stack() {
+		while (top != nullptr) {
+			Node* temp = top;
+			top = top->next;
+			delete temp;
+		}
+	}
+
+	void push(char c) {//入栈
+		Node* newNode = new Node(c);
+		newNode->next = top;
+		top = newNode;
+	}
+
+	char pop() {//出栈
+		if (isEmpty()) {
+			return '\0'; // 返回栈中的空字符
+		}
+		char c = top->data;
+		Node* temp = top;
+		top = top->next;
+		delete temp;
+		return c;
+	}
+
+	char peek() {
+		if (isEmpty()) {
+			return '\0'; // 返回栈中的空字符
+		}
+		return top->data;//返回栈顶元素
+	}
+
+	bool isEmpty() {//判断栈中是否为空
+		return top == nullptr;
+	}
+};
