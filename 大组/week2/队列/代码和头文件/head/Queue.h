a5a674be46722e50a289ac4c63fcc43f5600c67e
Nanbeer <161316818+Nanbeer@users.noreply.github.com>
2024-03-25 13:22:16 +0800

    2

--- 大组/week2/队列/代码和头文件/head/Queue.h
+++ 大组/week2/队列/代码和头文件/head/Queue.h
@@ -6,12 +6,15 @@ struct Node {
     void* data;
     Node* next;
     Node(void* d) : data(d), next(nullptr) {}  // 定义节点结构体，并初始化数据和下一个节点指针
+
 };
 
 class Queue {
 private:
     Node* front;
     Node* rear;
+
+
 
 public:
     Queue() : front(nullptr), rear(nullptr) {}  // 定义队列类，并初始化队首和队尾指针为空
@@ -21,18 +24,22 @@ public:
             Node* temp = front;
             front = front->next;
             delete temp;
+
         }
     }
+
 
     void enqueue(void* data) {  // 将数据入队
         Node* newNode = new Node(data);  // 创建一个新节点
         if (rear == nullptr) {  // 如果队列为空
             front = newNode;  // 新节点同时作为队首和队尾
             rear = newNode;
+
         }
         else {
             rear->next = newNode;  // 新节点加入到队尾，并更新队尾指针
             rear = newNode;
+
         }
     }
 
@@ -40,12 +47,15 @@ public:
         if (front == nullptr) {  // 如果队列为空
             return nullptr;
         }
+
         Node* temp = front;  // 保存队首节点
         void* data = temp->data;  // 获取队首节点的数据
         front = front->next;  // 更新队首指针
+
         if (front == nullptr) {  // 如果队列变空
             rear = nullptr;
         }
+
         delete temp;  // 删除原来的队首节点
         return data;  // 返回队首数据
     }
--- 大组/week2/队列/代码和头文件/queue/Queue.cpp
+++ 大组/week2/队列/代码和头文件/queue/Queue.cpp
@@ -12,15 +12,19 @@ int main() {
             break;
         }
 
+
         string* newData = new string(input);  // 创建新的字符串数据，并存入队列
         myQueue.enqueue(newData);
+
     }
 
     while (!myQueue.isEmpty()) {  // 循环直到队列为空
         string* dequeuedData = static_cast<string*>(myQueue.dequeue());  // 从队列中取出数据
         cout << "队列中的数据是: " << *dequeuedData << endl;  // 输出取出的数据
         delete dequeuedData;  // 释放数据的内存
+
     }
+
 
     return 0;
 }
