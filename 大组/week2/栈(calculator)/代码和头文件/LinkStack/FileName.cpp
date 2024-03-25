82f2545c0905f732c5e0cc8276e58547784f6e03
Nanbeer <161316818+Nanbeer@users.noreply.github.com>
2024-03-25 16:13:08 +0800

    Update FileName.cpp

--- 大组/week2/栈(calculator)/代码和头文件/LinkStack/FileName.cpp
+++ 大组/week2/栈(calculator)/代码和头文件/LinkStack/FileName.cpp
@@ -10,28 +10,29 @@ int precedence(char op) {//四则运算优先级判断
 	return 0;
 }
 
-string infixToPostfix(const string& infix) {//将中缀表达式转换为后缀表达式
+string infix(const string& infix) {//将中缀表达式转换为后缀表达式
 	string pos;//储存后缀表达式
-	Stack opStack;
+	Stack operStack;
+
 
 	for (char c : infix) {//遍历中缀表达式
 		if (isdigit(c)) {//判断是不是0-9的数字  使用isdigit函数
 			pos += c;
 		}
 		else if (c == '(') {//判断符号（）:括号的优先级最高 
-			opStack.push(c);//入栈
+			operStack.push(c);//入栈
 		}
 		else if (c == ')') {
-			while (!opStack.isEmpty() && opStack.peek() != '(') {//当栈不为空且栈顶元素不是'('时
-				pos += opStack.pop();//将括号中的出栈
+			while (!operStack.isEmpty() && operStack.peek() != '(') {//当栈不为空且栈顶元素不是'('时
+				pos += operStack.pop();//将括号中的出栈
 			}
-			opStack.pop(); //把'('出栈
+			operStack.pop(); //把'('出栈
 		}
 		else if (c == '+' || c == '-' || c == '*' || c == '/') {
-			while (!opStack.isEmpty() && precedence(opStack.peek()) >= precedence(c)) {//比较栈顶元素和当前操作符的优先级
-				pos += opStack.pop();
+			while (!operStack.isEmpty() && precedence(operStack.peek()) >= precedence(c)) {//比较栈顶元素和当前操作符的优先级
+				pos += operStack.pop();
 			}
-			opStack.push(c);
+			operStack.push(c);
 		}
 		else {
 			cout << "输入有误，请重新输入" << endl;
@@ -41,8 +42,8 @@ string infixToPostfix(const string& infix) {//将中缀表达式转换为后缀表达式
 	
 
 
-	while (!opStack.isEmpty()) {//清空操作符栈，并将栈中剩余的操作符按正确的顺序追加到后缀表达式字符串postfix中
-		pos += opStack.pop();
+	while (!operStack.isEmpty()) {//清空操作符栈，并将栈中剩余的操作符按正确的顺序追加到后缀表达式字符串postfix中
+		pos += operStack.pop();
 	}
 
 
@@ -52,32 +53,32 @@ string infixToPostfix(const string& infix) {//将中缀表达式转换为后缀表达式
 
 }
 
-int evaluatePostfix(const string& pos) {//使用后缀表达式进行运算
-	Stack operandStack;
+int evaluate(const string& pos) {//使用后缀表达式进行运算
+	Stack operStack;
 
 	for (char c : pos) {
 		if (isdigit(c)) {
-			operandStack.push(c - '0');//将0-9的字符转换为int
+			operStack.push(c - '0');//将0-9的字符转换为int
 		}
 		else if (c == '+' || c == '-' || c == '*' || c == '/') {
-			int operand2 = operandStack.pop();//最上面的两个元素出栈
-			int operand1 = operandStack.pop();
+			int oper2 = operStack.pop();//最上面的两个元素出栈
+			int oper1 = operStack.pop();
 			int result;
 			switch (c) {
 			case '+':
-				result = operand1 + operand2;
+				result = oper1 + oper2;
 				break;
 			case '-':
-				result = operand1 - operand2;
+				result = oper1 - oper2;
 				break;
 			case '*':
-				result = operand1 * operand2;
+				result = oper1 * oper2;
 				break;
 			case '/':
-				result = operand1 / operand2;
+				result = oper1 / oper2;
 				break;
 			}
-			operandStack.push(result);//每一次计算结果入栈
+			operStack.push(result);//每一次计算结果入栈
 		}
 		else {
 			cout << "输入有误，请重新输入" << endl;
@@ -87,7 +88,7 @@ int evaluatePostfix(const string& pos) {//使用后缀表达式进行运算
 
 
 
-	return operandStack.pop();//结果出栈
+	return operStack.pop();//结果出栈
 
 
 }
@@ -100,12 +101,12 @@ int main() {
 		cout << "请输入中缀表达式（例如 3+4*2）: ";
 		cin >> infixExpression;
 
-		string postfixExpression = infixToPostfix(infixExpression);
+		string postfixExpression = infix(infixExpression);
 
 		if (!postfixExpression.empty()) {
 			cout << "后缀表达式为: " << postfixExpression << endl;
 
-			int result = evaluatePostfix(postfixExpression);
+			int result = evaluate(postfixExpression);
 			cout << "计算结果为: " << result << endl;
 		}
 
